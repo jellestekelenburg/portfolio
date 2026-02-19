a66e817ffced20d98cd68580855fa2023da6c15d
jellestekelenburg <jellemaarten02@gmail.com>
2026-02-19 16:50:48 +0100

    color fix

---
+++ resources/js/components/ButtonSecondary.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+    href?: string;
+}>();
+</script>
+
+<template>
+    <component
+        :is="href ? 'a' : 'button'"
+        :href="href"
+        class="inline-block px-6 py-2.5 border border-white/20 hover:border-white/40 text-white font-medium rounded-lg transition-colors duration-200"
+    >
+        <slot />
+    </component>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
-require __DIR__.'/settings.php';
+//require __DIR__.'/settings.php';
