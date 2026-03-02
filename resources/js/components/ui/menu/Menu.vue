151b7d2927ba31fac360a1065e118cc6e384a8d6
jellestekelenburg <jellemaarten02@gmail.com>
2026-03-02 19:13:18 +0100

    logo fixes

--- resources/js/components/ui/menu/Menu.vue
+++ resources/js/components/ui/menu/Menu.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 
+import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
     <ul class="flex items-center gap-x-6 text-sm  text-gray-700 dark:text-white/80">
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Projects</a></li>
-        <li><a href="/">Contact</a></li>
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Projects</Link>
+        <Link href="/">Contact</Link>
     </ul>
 </template>
