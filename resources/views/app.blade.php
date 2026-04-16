8abe544dcb846d52a8730dfa918ee1e15618a241
jellestekelenburg <jellemaarten02@gmail.com>
2026-04-16 13:18:06 +0200

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,6 +11,8 @@
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
         rel="stylesheet"
     >
+    <link rel="icon" type="image/x-icon" href="/favicon/favicon.ico">
+    <link rel="icon" type="image/svg" href="/favicon/favicon.svg">
 
     @vite(['resources/js/app.ts'])
 
