58cf74cb99a15a8a522ccd4d314c2ae7777ad917
jellestekelenburg <jellemaarten02@gmail.com>
2026-04-16 15:44:15 +0200

    coming soon page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+    return Inertia::render('ComingSoon');
 })->name('home');
 
 //require __DIR__.'/settings.php';
