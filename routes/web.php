e0a7540427f1d495e5d6604093925fbf5fa012fd
jellestekelenburg <jellemaarten02@gmail.com>
2026-06-18 15:49:05 +0200

    route fixing

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('ComingSoon');
+    return Inertia::render('Home');
 })->name('home');
 
-Route::get('/{any}', function () {
-    return Inertia::render('ComingSoon');
-});
+// Route::get('/{any}', function () {
+//    return Inertia::render('ComingSoon');
+// });
 
-//require __DIR__.'/settings.php';
+// require __DIR__.'/settings.php';
