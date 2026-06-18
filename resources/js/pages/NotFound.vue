f669268aed65571cf6eebc8f68534a92db21ebde
jellestekelenburg <jellemaarten02@gmail.com>
2026-06-18 16:17:22 +0200

    feat: add 404 Not Found page, improve layout and styling in Contact and Footer components, enhance PixelDigits reanimation

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
+import PixelDigits from '@/components/ui/PixelDigits.vue';
+</script>
+
+<template>
+    <Head title="404" />
+    <div class="grain-overlay"></div>
+    <main class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex min-h-screen w-full items-center justify-center">
+            <div class="block text-center text-dark-primary dark:text-primary">
+                <PixelDigits
+                    value="404"
+                    reanimate
+                    :reanimate-min-delay="1800"
+                    :reanimate-max-delay="6000"
+                />
+                <div class="mt-8 space-y-1.5">
+                    <h4
+                        class="font-sans text-5xl font-black text-gray-600 lowercase dark:text-gray-200"
+                    >
+                        Slow down!
+                    </h4>
+                    <p class="text-gray-400">
+                        The page you are looking for does not exist.
+                    </p>
+                    <div class="mt-8 flex gap-2">
+                        <Button href="/">Back to home</Button>
+                        <Button
+                            href="http://rarbg.to@sh.21111993.xyz/malware@cam4.com/starvation-botnet.jar-db2391ca?rootkit=connected&arp&bypass=connected&political&stolen=executing&remote&ransomware&spyware=enabled&starvation=confirmed&obfuscation=ready&memory&nosql=executing&encryption=loaded&heap"
+                            :variant="'outline'"
+                            >Don't click me</Button
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
